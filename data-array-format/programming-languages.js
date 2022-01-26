704958539cd7f5a9a066b329911274d49b68ad57
unknown <suong.liang@gmail.com>
2022-01-26 18:13:32 +0700

    add languages programming list

---
+++ data-array-format/programming-languages.js
@@ -0,0 +1,318 @@
+export const AVAILABLE_PROGRAMMING_LANGUAGES = [
+    {
+        label: 'ABAP',
+        codeAbbr: 'abap',
+    },
+    {
+        label: 'Arduino',
+        codeAbbr: 'arduino',
+    },
+    {
+        label: 'Bash',
+        codeAbbr: 'bash',
+    },
+    {
+        label: 'Bash',
+        codeAbbr: 'bash',
+    },
+    {
+        label: 'BASIC',
+        codeAbbr: 'basic',
+    },
+    {
+        label: 'C',
+        codeAbbr: 'c',
+    },
+    {
+        label: 'Clojure',
+        codeAbbr: 'clojure',
+    },
+    {
+        label: 'CoffeeScript',
+        codeAbbr: 'coffeescript',
+    },
+    {
+        label: 'C++',
+        codeAbbr: 'cpp',
+    },
+    {
+        label: 'C#',
+        codeAbbr: 'csharp',
+    },
+    {
+        label: 'CSS',
+        codeAbbr: 'css',
+    },
+    {
+        label: 'Dart',
+        codeAbbr: 'dart',
+    },
+    {
+        label: 'Dart',
+        codeAbbr: 'dart',
+    },
+    {
+        label: 'Diff',
+        codeAbbr: 'diff',
+    },
+    {
+        label: 'Docker',
+        codeAbbr: 'dockerfile',
+    },
+    {
+        label: 'Elixir',
+        codeAbbr: 'elixir',
+    },
+    {
+        label: 'Docker',
+        codeAbbr: 'dockerfile',
+    },
+    {
+        label: 'Elm',
+        codeAbbr: 'elm',
+    },
+    {
+        label: 'Erlang',
+        codeAbbr: 'erlang',
+    },
+    {
+        label: 'Erlang',
+        codeAbbr: 'erlang',
+    },
+    {
+        label: 'Flow',
+        codeAbbr: 'flow',
+    },
+    {
+        label: 'Fortran',
+        codeAbbr: 'fortran',
+    },
+    {
+        label: 'F#',
+        codeAbbr: 'fsharp',
+    },
+    {
+        label: 'Gherkin',
+        codeAbbr: 'gherkin',
+    },
+    {
+        label: 'GLSL',
+        codeAbbr: 'glsl',
+    },
+    {
+        label: 'Go',
+        codeAbbr: 'go',
+    },
+    {
+        label: 'GraphQL',
+        codeAbbr: 'graphql',
+    },
+    {
+        label: 'Groovy',
+        codeAbbr: 'groovy',
+    },
+    {
+        label: 'Haskell',
+        codeAbbr: 'haskell',
+    },
+    {
+        label: 'HTML',
+        codeAbbr: 'html',
+    },
+    {
+        label: 'Java',
+        codeAbbr: 'java',
+    },
+    {
+        label: 'JavaScript',
+        codeAbbr: 'javascript',
+    },
+    {
+        label: 'JSON',
+        codeAbbr: 'json',
+    },
+    {
+        label: 'Julia',
+        codeAbbr: 'julia',
+    },
+    {
+        label: 'Kotlin',
+        codeAbbr: 'kotlin',
+    },
+    {
+        label: 'Julia',
+        codeAbbr: 'julia',
+    },
+    {
+        label: 'Julia',
+        codeAbbr: 'julia',
+    },
+    {
+        label: 'Julia',
+        codeAbbr: 'julia',
+    },
+    {
+        label: 'LaTeX',
+        codeAbbr: 'latex',
+    },
+    {
+        label: 'Less',
+        codeAbbr: 'less',
+    },
+    {
+        label: 'Lisp',
+        codeAbbr: 'lisp',
+    },
+    {
+        label: 'LiveScript',
+        codeAbbr: 'livescript',
+    },
+    {
+        label: 'Lua',
+        codeAbbr: 'lua',
+    },
+    {
+        label: 'Makefile',
+        codeAbbr: 'Makefile',
+    },
+    {
+        label: 'Markdown',
+        codeAbbr: 'markdown',
+    },
+    {
+        label: 'Markup',
+        codeAbbr: 'markup',
+    },
+    {
+        label: 'MATLAB',
+        codeAbbr: 'matlab',
+    },
+    {
+        label: 'Mermaid',
+        codeAbbr: 'mermaid',
+    },
+    {
+        label: 'Nix',
+        codeAbbr: 'nix',
+    },
+    {
+        label: 'Objective-C',
+        codeAbbr: 'objective-c',
+    },
+    {
+        label: 'OCaml',
+        codeAbbr: 'ocaml',
+    },
+    {
+        label: 'Pascal',
+        codeAbbr: 'pascal',
+    },
+    {
+        label: 'Perl',
+        codeAbbr: 'perl',
+    },
+    {
+        label: 'PHP',
+        codeAbbr: 'php',
+    },
+    {
+        label: 'Plain Text',
+        codeAbbr: 'plain text',
+    },
+    {
+        label: 'PowerShell',
+        codeAbbr: 'powershell',
+    },
+    {
+        label: 'Prolog',
+        codeAbbr: 'prolog',
+    },
+    {
+        label: 'Protobuf',
+        codeAbbr: 'protobuf',
+    },
+    {
+        label: 'Python',
+        codeAbbr: 'python',
+    },
+    {
+        label: 'R',
+        codeAbbr: 'r',
+    },
+    {
+        label: 'Reason',
+        codeAbbr: 'reason',
+    },
+    {
+        label: 'Ruby',
+        codeAbbr: 'ruby',
+    },
+    {
+        label: 'Rust',
+        codeAbbr: 'rust',
+    },
+    {
+        label: 'Sass',
+        codeAbbr: 'sass',
+    },
+    {
+        label: 'Scala',
+        codeAbbr: 'scala',
+    },
+    {
+        label: 'Scheme',
+        codeAbbr: 'scheme',
+    },
+    {
+        label: 'Scss',
+        codeAbbr: 'scss',
+    },
+    {
+        label: 'Shell',
+        codeAbbr: 'shell',
+    },
+    {
+        label: 'Solidity',
+        codeAbbr: 'solidity',
+    },
+    {
+        label: 'SQL',
+        codeAbbr: 'sql',
+    },
+    {
+        label: 'Swift',
+        codeAbbr: 'swift',
+    },
+    {
+        label: 'TypeScript',
+        codeAbbr: 'typescript',
+    },
+    {
+        label: 'VB.Net',
+        codeAbbr: 'vb',
+    },
+    {
+        label:  'Verilog',
+        codeAbbr: 'verilog',
+    },
+    {
+        label:  'VHDL',
+        codeAbbr: 'vhdl',
+    },
+    {
+        label:  'Visual Basic',
+        codeAbbr: 'vbscript',
+    },
+    {
+        label:  'WebAssembly',
+        codeAbbr: 'wasm',
+    }, 
+    {
+        label:  'XML',
+        codeAbbr: 'xml',
+    },
+    {
+        label:  'YAML',
+        codeAbbr: 'yaml',
+    },
+];
